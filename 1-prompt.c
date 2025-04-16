5e46fc7a7fa89a6fc0778a2d2b62b89077069c21
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-16 13:11:03 -0600

    prompt

--- 1-prompt.c
+++ 1-prompt.c
@@ -1,55 +1,62 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <string.h>
+
+extern char **environ;
 
 /**
  * main - Simple shell
- * Return: 0 on succes, 1 on error
+ * Return: 0 on success, 1 on error
  */
 int main(void)
 {
-	char *line = NULL;
-	char *args[100];
-	char *token = strtok(line, " ");
-	size_t len = 0;
-	ssize_t read;
-	int i = 0;
-	pid_t pid;
+    char *line = NULL;
+    char *args[100];
+    size_t len = 0;
+    ssize_t read;
+    pid_t pid;
 
-	while (1)
-	{
-		write(1, "#cisfun$ ", 9);
+    while (1)
+    {
+        write(1, "#cisfun$ ", 9);
 
-		read = getline(&line, &len, stdin);
+        read = getline(&line, &len, stdin);
 
-		if (read == -1)
-		{
-			free(line);
-			exit(0);
-		}
+        if (read == -1)
+        {
+            free(line);
+            exit(0);
+        }
 
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
 
-		while (token != NULL && i < 99)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
+        int i = 0;
+        char *token = strtok(line, " ");
+        while (token != NULL && i < 99)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[i] = NULL;
 
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(line, NULL, environ) == -1)
-				perror("./shell");
-			exit(1);
-		}
-		else if (pid > 0)
-			wait(NULL);
-		else
-			perror("Error:");
-	}
+        pid = fork();
+        if (pid == 0)
+        {
+            if (execve(args[0], args, environ) == -1)
+                perror("./shell");
+            exit(1);
+        }
+        else if (pid > 0)
+            wait(NULL);
+        else
+            perror("Error:");
+    }
 
-	free(line);
-	return (0);
+    free(line);
+    return (0);
 }
