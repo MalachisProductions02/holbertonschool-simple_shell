3cbed2595fc72ac2ca595dd29b87a7bf3e5f617b
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-16 00:32:11 +0000

    main

--- main.h
+++ main.h
@@ -1,11 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 /**
  * int prototype
  * void prototype
  * char prototype
  */
+
+extern char **environ;
+
 int _putchar(char);
 int add_numbers(int a, int b);
 
