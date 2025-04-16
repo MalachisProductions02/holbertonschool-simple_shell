ff3d897ca08fe7e22ec245440b47fe2df7198e28
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-16 00:02:41 +0000

    add numbers

---
+++ 0-add_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add_numbers - Adds two integers
+ * @a: First number
+ * @b: Second number
+ * Return: sum of a and b
+ */
+int add_numbers(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
