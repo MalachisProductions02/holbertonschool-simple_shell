7c675cc185931618cd3316be4bd2807124ef7189
Luis May <a20214976@alumnos.uady.mx>
2025-04-17 07:07:55 -0600

    fix: refactor execute_command to tokenize the input

--- execute_command.c
+++ execute_command.c
@@ -6,37 +6,46 @@ char *get_full_path(char *command);
  * execute_command - executes a command (with or without PATH)
  * @command: the command to run (e.g. ls, /bin/ls, ./hbtn_ls)
  */
-void execute_command(char *command)
+void execute_command(char *line)
 {
-	pid_t pid = fork();
+	pid_t pid;
+	char *token, *cmd_to_exec;
+	char *argv[MAX_ARGS];
+	int i = 0;
 
+	/* Tokenize input into argv */
+	token = strtok(line, " ");
+	while (token && i < MAX_ARGS - 1)
+	{
+		argv[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	argv[i] = NULL;
+
+	if (argv[0] == NULL)
+		return;
+
+	pid = fork();
 	if (pid == 0)
 	{
-		char *argv[2];
-		char *cmd_to_exec;
-
-		argv[0] = command;
-		argv[1] = NULL;
-
-		if (strchr(command, '/') != NULL)
+		if (strchr(argv[0], '/') != NULL)
 		{
-			cmd_to_exec = command;
+			cmd_to_exec = argv[0];
 		}
 		else
 		{
-			cmd_to_exec = get_full_path(command);
-			if (cmd_to_exec == NULL)
+			cmd_to_exec = get_full_path(argv[0]);
+			if (!cmd_to_exec)
 			{
-				fprintf(stderr, "./shell: command not found: %s\n", command);
+				fprintf(stderr, "%s: command not found\n", argv[0]);
 				exit(EXIT_FAILURE);
 			}
 		}
 
 		if (execve(cmd_to_exec, argv, environ) == -1)
 		{
-            /* this helps to use others paths */
 			perror(cmd_to_exec);
-			if (cmd_to_exec != command)
+			if (cmd_to_exec != argv[0])
 				free(cmd_to_exec);
 			exit(EXIT_FAILURE);
 		}
@@ -44,7 +53,6 @@ void execute_command(char *command)
 	else if (pid > 0)
 	{
 		int status;
-
 		waitpid(pid, &status, 0);
 	}
 	else
