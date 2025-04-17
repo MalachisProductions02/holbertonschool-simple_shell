a52d7407ceed42425e8238ff68d24e3745d29a85
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-17 02:43:37 +0000

    main

--- main.h
+++ main.h
@@ -1,11 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define MAX_ARGS 64
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
 /**
  * int prototype
@@ -14,6 +17,8 @@
  */
 
 extern char **environ;
+
+void simple_shell(void);
 
 int _putchar(char);
 int add_numbers(int a, int b);
