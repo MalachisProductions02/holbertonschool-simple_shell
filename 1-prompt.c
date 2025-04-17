a91c4ce10dd444c142cce456d5049822648fdc7a
Luis May <a20214976@alumnos.uady.mx>
2025-04-17 05:20:24 -0600

    fix: comment out the main function and related code in 1-prompt.c for future reference

--- 1-prompt.c
+++ 1-prompt.c
@@ -1,75 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <sys/wait.h>
+// #include <string.h>
 
-extern char **environ;
+// extern char **environ;
 
-/**
- * main - Simple Shell replica
- * Return: Always 0.
- */
-int main(void)
-{
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
-    char *args[100];
-    char *token;
-    pid_t pid;
-    int status;
+// /**
+//  * main - Simple Shell replica
+//  * Return: Always 0.
+//  */
+// int main(void)
+// {
+//     char *line = NULL;
+//     size_t len = 0;
+//     ssize_t read;
+//     char *args[100];
+//     char *token;
+//     pid_t pid;
+//     int status;
 
-    while (1)
-    {
-        int i;
+//     while (1)
+//     {
+//         int i;
 
-        if (isatty(STDIN_FILENO))
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
+//         if (isatty(STDIN_FILENO))
+//             write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            free(line);
-            break;
-        }
+//         read = getline(&line, &len, stdin);
+//         if (read == -1)
+//         {
+//             free(line);
+//             break;
+//         }
 
-        /* Remover salto de línea */
-        if (line[read - 1] == '\n')
-            line[read - 1] = '\0';
+//         /* Remover salto de línea */
+//         if (line[read - 1] == '\n')
+//             line[read - 1] = '\0';
 
-        /* Tokenizar entrada */
-        i = 0;
-        token = strtok(line, " ");
-        while (token != NULL && i < 99)
-        {
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[i] = NULL;
+//         /* Tokenizar entrada */
+//         i = 0;
+//         token = strtok(line, " ");
+//         while (token != NULL && i < 99)
+//         {
+//             args[i++] = token;
+//             token = strtok(NULL, " ");
+//         }
+//         args[i] = NULL;
 
-        if (args[0] == NULL)
-            continue;
+//         if (args[0] == NULL)
+//             continue;
 
-        pid = fork();
-        if (pid == 0)
-        {
-            if (execve(args[0], args, environ) == -1)
-            {
-                perror("./shell");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else if (pid > 0)
-        {
-            wait(&status);
-        }
-        else
-        {
-            perror("fork");
-        }
-    }
+//         pid = fork();
+//         if (pid == 0)
+//         {
+//             if (execve(args[0], args, environ) == -1)
+//             {
+//                 perror("./shell");
+//                 exit(EXIT_FAILURE);
+//             }
+//         }
+//         else if (pid > 0)
+//         {
+//             wait(&status);
+//         }
+//         else
+//         {
+//             perror("fork");
+//         }
+//     }
 
-    free(line);
-    return 0; /* Times that I corrected this code: 21...damn... */
-}
+//     free(line);
+//     return 0; /* Times that I corrected this code: 21...damn... */
+// }
