466d6b8de95abe2740708bef07d7552c9b4deec6
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-17 01:43:10 +0000

    simple shell 0.2

---
+++ 2-simple_shell02.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+extern char **environ;
+
+#define MAX_ARGS 64
+
+/**
+ * main - Simple shell 0.1+ : executes commands with arguments
+ * Return: Always 0, succes
+ */
+int main(void)
+{
+	char *line = NULL, *token, *args[MAX_ARGS];
+	size_t len = 0;
+	ssize_t read;
+	pid_t pid;
+	int status, i;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+		read = getline(&line, &len, stdin);
+		if (read == -1)
+			break;
+
+		if (line[read - 1] == '\n')
+			line[read  - 1] = '\0';
+
+		i = 0;
+		token = strtok(line, " ");
+		while (token && i < MAX_ARGS - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+
+		if (args[0] == NULL)
+			continue;
+
+		if (strcmp(args[0], "exit") == 0)
+			break;
+
+		if (strcmp(args[0], "env") == 0)
+		{
+			for (i = 0; environ[i]; i++)
+				printf("%s\n", environ[i]);
+			continue;
+		}
+
+		pid = fork();
+		if (pid = 0)
+		{
+			if (execve(args[0], args, environ) == -1)
+			{
+				perror("./hsh");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else if (pid > 0)
+		{
+			waitpid(pid, &status, 0);
+		}
+		else
+		{
+			perror("fork");
+		}
+	}
+
+	free(line);
+	return (0);
+}
