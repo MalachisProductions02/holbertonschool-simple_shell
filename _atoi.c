de43a4641705531e475fc904ec27daa6b7d246ce
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-18 21:21:59 +0000

    atoi

---
+++ _atoi.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The int value of the string
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1;
+	unsigned int num = 0;
+
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + (s[i] - '0');
+		i++;
+	}
+
+	return (num * sign);
+}
