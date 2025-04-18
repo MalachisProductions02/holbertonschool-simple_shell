9cb1332830a8ef14bda0c0446554e5fa0e338345
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-18 14:34:29 -0600

    getline

--- getline.c
+++ getline.c
@@ -5,39 +5,43 @@
 
 ssize_t custom_getline(char **lineptr, size_t *n, int fd)
 {
-	static char buffer[BUFFER_SIZE];
-	static size_t buf_pos, buf_size;
-	char *new_line;
-	char c;
-	ssize_t len = 0;
+    static char buffer[BUFFER_SIZE];
+    static size_t buf_pos, buf_size;
+    char *new_line;
+    char c;
+    ssize_t len = 0;
 
-	if (!lineptr || !n) return -1;
-	if (*lineptr == NULL || *n == 0) {
-		*n = 128;
-		*lineptr = malloc(*n);
-		if (!*lineptr) return -1;
-	}
+    if (!lineptr || !n) return -1;
+    if (*lineptr == NULL || *n == 0) {
+        *n = 128;
+        *lineptr = malloc(*n);
+        if (!*lineptr) return -1;
+    }
 
-	while (1) {
-		if (buf_pos >= buf_size) {
-			buf_size  = read(fd, buffer, BUFFER_SIZE);
-			if (buf_size <= 0) return (len > 0) ? len : -1;
-			buf_pos = 0;
-		}
+    while (1) {
+        if (buf_pos >= buf_size) {
+            buf_size = read(fd, buffer, BUFFER_SIZE);
+            if (buf_size <= 0) return (len > 0) ? len : -1;
+            buf_pos = 0;
+        }
 
-		c = buffer[buf_pos++];
-		(*lineptr)[len++] = c;
+        c = buffer[buf_pos++];
+        (*lineptr)[len++] = c;
 
-		if ((size_t)len >= *n) {
-			*n *= 2;
-			new_line = realloc(*lineptr, *n);
-			if (!new_line) return -1;
-			*lineptr = new_line;
-		}
+        if ((size_t)len >= *n) {
+            *n *= 2;
+            new_line = malloc(*n);
+            if (!new_line) return -1;
+            for (size_t i = 0; i < len; i++) {
+                new_line[i] = (*lineptr)[i];
+            }
+            free(*lineptr);
+            *lineptr = new_line;
+        }
 
-		if (c == '\n') break;
-	}
+        if (c == '\n') break;
+    }
 
-	(*lineptr)[len] = '\0';
-	return len;
+    (*lineptr)[len] = '\0';
+    return len;
 }
