c1b560fab0331b7d13e2570f5c94fdabd8586d08
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-18 14:08:56 -0600

    utils

--- utils.c
+++ utils.c
@@ -54,7 +54,7 @@ void handle_env(void)
 
 	while (environ[i])
 	{
-		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
 		write(STDOUT_FILENO, "\n", 1);
 		i++;
 	}
