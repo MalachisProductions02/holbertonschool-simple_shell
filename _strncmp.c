601a342a63bd8f8330cb8ab0fd43901f54cb5d77
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-19 19:22:08 +0000

    strncmp

---
+++ _strncmp.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+
+/**
+ * _strncmp - Compare two strings up to n characters
+ * @s1: First string
+ * @s2: Second string
+ * @n: Max number of characters to compare
+ *
+ * Return: 0 if strings are equal up to n, difference between first differing chars otherwise
+ */
+int _strncmp(const char *s1, const char *s2, size_t n)
+{
+    while (n && *s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+        n--;
+    }
+    if (n == 0)
+        return (0);
+    return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
