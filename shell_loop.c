01a898c5791c9a1255d3e0a40598ace3d91d450c
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-19 16:27:45 -0600

    shell loop

--- shell_loop.c
+++ shell_loop.c
@@ -1,48 +1,158 @@
 #include "shell.h"
-#include <limits.h>
 
 /**
- * builtin_cd - Changes the current directory of the process
- * @args: argument vector, args[0] is "cd", args[1] is the target directory
- * Return: 0 on success, -1 on failure
+ * trim_whitespace - Removes leading and trailing whitespace from a string
+ * @str: The input string to be trimmed
+ *
+ * Return: Pointer to the trimmed string (within original buffer)
  */
-int builtin_cd(char **args)
+char *trim_whitespace(char *str)
 {
-    char *dir, *home, *oldpwd, cwd[PATH_MAX];
+    char *end;
 
-    oldpwd = getcwd(cwd, sizeof(cwd));
+    while (*str == ' ')
+        str++;
 
-    if (args[1] == NULL)
+    if (*str == '\0')
+        return (str);
+
+    end = str + _strlen(str) - 1;
+
+    while (end > str && (*end == ' ' || *end == '\n'))
+        end--;
+
+    *(end + 1) = '\0';
+
+    return (str);
+}
+
+/**
+ * shell_loop - Main loop of the simple shell
+ * Displays prompt, reads input, and executes command
+ */
+void shell_loop(void)
+{
+    char *line = NULL;
+    char *trimmed = NULL;
+    char **args = NULL;
+    size_t len = 0;
+    ssize_t read;
+    int status = 0;
+    int i;
+
+    while (1)
     {
-        home = _getenv("HOME");
-        if (!home)
-            return (-1);
-        if (chdir(home) != 0)
-            return (-1);
-    }
-    else if (_strcmp(args[1], "-") == 0)
-    {
-        char *old = _getenv("OLDPWD");
-        if (!old)
-            return (-1);
-        if (chdir(old) != 0)
-            return (-1);
-        write(STDOUT_FILENO, old, _strlen(old));
-        write(STDOUT_FILENO, "\n", 1);
-    }
-    else
-    {
-        dir = args[1];
-        if (chdir(dir) != 0)
-            return (-1);
-    }
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "#cisfun$ ", 9);
 
-    setenv("OLDPWD", oldpwd, 1);
+        read = custom_getline(&line, &len, STDIN_FILENO);
+        if (read == -1)
+        {
+            if (isatty(STDIN_FILENO))
+                write(STDOUT_FILENO, "\n", 1);
+            break;
+        }
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        setenv("PWD", cwd, 1);
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
+
+        trimmed = trim_whitespace(line);
+        if (trimmed[0] == '\0')
+            continue;
+
+        args = split_line(trimmed);
+
+        if (args && args[0] && args[0][0] != '\0')
+        {
+            if (_strcmp(args[0], "exit") == 0)
+            {
+                if (args[1] != NULL)
+                {
+                    int valid = 1;
+
+                    i = 0;
+                    while (args[1][i])
+                    {
+                        if (args[1][i] < '0' || args[1][i] > '9')
+                        {
+                            if (args[1][i] == '-' && i == 0 && args[1][i + 1] != '\0')
+                            {
+                                write(2, "./hsh: 1: exit: Illegal number: ", 32);
+                                write(2, args[1], _strlen(args[1]));
+                                write(2, "\n", 1);
+                            }
+                            else
+                            {
+                                write(2, "exit: numeric argument required\n", 32);
+                            }
+                            valid = 0;
+                            break;
+                        }
+                        i++;
+                    }
+                    if (valid)
+                        status = _atoi(args[1]);
+                    else
+                        status = 2;
+                }
+                else
+                {
+                    status = 0;
+                }
+
+                for (i = 0; args[i] != NULL; i++)
+                    free(args[i]);
+                free(args);
+                free(line);
+                exit(status);
+            }
+            else if (_strcmp(args[0], "env") == 0)
+            {
+                handle_env();
+            }
+            else if (_strcmp(args[0], "setenv") == 0)
+            {
+                if (args[1] && args[2])
+                {
+                    if (_setenv(args[1], args[2], 1) != 0)
+                        write(2, "Failed to set environment variable\n", 35);
+                }
+                else
+                {
+                    write(2, "Usage: setenv VARIABLE VALUE\n", 29);
+                }
+            }
+            else if (_strcmp(args[0], "unsetenv") == 0)
+            {
+                if (args[1])
+                {
+                    if (_unsetenv(args[1]) != 0)
+                        write(2, "Failed to unset environment variable\n", 37);
+                }
+                else
+                {
+                    write(2, "Usage: unsetenv VARIABLE\n", 25);
+                }
+            }
+            else if (_strcmp(args[0], "cd") == 0)  /* NUEVA PARTE */
+            {
+                if (builtin_cd(args) != 0)
+                    status = 2;
+            }
+            else
+            {
+                last_status = execute_command(trimmed);
+                status = (last_status != 0) ? last_status : 0;
+            }
+        }
+
+        if (args != NULL)
+        {
+            for (i = 0; args[i] != NULL; i++)
+                free(args[i]);
+            free(args);
+        }
     }
-
-    return (0);
+    free(line);
+    exit(status);
 }
