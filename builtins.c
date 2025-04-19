fb91090a70eb7e0c382c606c43865ea334ecfbe6
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-19 13:18:17 -0600

    builtins

--- builtins.c
+++ builtins.c
@@ -20,16 +20,22 @@
 int _setenv(const char *name, const char *value, int overwrite)
 {
     char *new_var;
-    int name_len, value_len;
+    int i = 0, name_len = _strlen(name), value_len = _strlen(value);
 
-    if (!name || !value || name[0] == '\0')
+    if (!name || !value || name[0] == '\0' || _strchr(name, '=') != NULL)
         return (-1);
 
-    if (getenv(name) != NULL && overwrite == 0)
-        return (0);
-
-    name_len = _strlen(name);
-    value_len = _strlen(value);
+    while (environ[i])
+    {
+        if (_strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
+        {
+            if (!overwrite)
+                return (0);
+            free(environ[i]);
+            break;
+        }
+        i++;
+    }
 
     new_var = malloc(name_len + value_len + 2);
     if (!new_var)
@@ -38,16 +44,12 @@ int _setenv(const char *name, const char *value, int overwrite)
     _strcpy(new_var, name);
     new_var[name_len] = '=';
     _strcpy(new_var + name_len + 1, value);
+    new_var[name_len + value_len + 1] = '\0';
 
-    if (putenv(new_var) != 0)
-    {
-        free(new_var);
-        return (-1);
-    }
-
+    environ[i] = new_var;
+    environ[i + 1] = NULL;
     return (0);
 }
-
 /**
  * _unsetenv - Removes an environment variable
  * @name: Name of the variable to remove
@@ -55,21 +57,22 @@ int _setenv(const char *name, const char *value, int overwrite)
  */
 int _unsetenv(const char *name)
 {
-    int i, j, len_name;
+    int i = 0, j;
+    size_t len = _strlen(name);
 
-    if (!name || name[0] == '\0')
+    if (!name || name[0] == '\0' || _strchr(name, '=') != NULL)
         return (-1);
 
-    len_name = strlen(name);
-
-    for (i = 0; environ[i]; i++)
+    while (environ[i])
     {
-        if (strncmp(environ[i], name, len_name) == 0 && environ[i][len_name] == '=')
+        if (_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
         {
+            free(environ[i]);
             for (j = i; environ[j]; j++)
                 environ[j] = environ[j + 1];
             return (0);
         }
+        i++;
     }
     return (0);
 }
